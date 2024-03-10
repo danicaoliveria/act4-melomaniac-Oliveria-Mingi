332aa9ece0a0f868d2141a8f7fa9171e1f9f1f61
OliveriaDanica <oliveriadanica@gmail.com>
2024-03-10 21:21:49 +0800

    song2 with lyrics

---
+++ song2.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>song1</title>
+    <style>
+        body {
+            display: flex;
+            font-size: 16px;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin: 0;
+            padding: 0;
+            font-family: "Times New Roman", Times, serif;;
+            background-color: lavender;
+        }
+
+        .content {
+            text-align: center;
+        }
+        .song-title {
+            margin-bottom: -30px;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+<pre>
+    <h1 class="song-title"> One </h1>
+   <h2 style="font-size:16px;">song by Ed Sheeran</h2>
+  <?php   
+    echo "<p>[Verse]</p>";
+    echo "<p>Am           F            C                G</p>";
+    echo "<p>Tell me that you turned down the man</p>";
+    echo "<p>Am           F              C             G</p>";
+    echo "<p>Who asked for your hand</p>";
+    echo "<p>Am            F            C                   G</p>";
+    echo "<p>'Cause you're waiting for me</p>";
+    echo "<p>Am               F              C             G</p>";
+    echo "<p>And I know, you're gonna be away a while</p>";
+    echo "<p>Am            F            C                G</p>";
+    echo "<p>But I've got no plans at all to leave</p>";
+    echo "<p>Am              F              C               G</p>";
+    echo "<p>And would you take away my hopes and dreams</p>";
+    echo "<p>Am           F                 C            G</p>";
+    echo "<p>Just stay with me</p>";
+    echo "<p>[Chorus]</p>";
+    echo "<p>Am                    F</p>";
+    echo "<p>All my senses come to life</p>";
+    echo "<p>C                G</p>";
+    echo "<p>While I'm stumbling home as drunk as I</p>";
+    echo "<p>Am              F</p>";
+    echo "<p>Have ever been</p>";
+    echo "<p>C              G</p>";
+    echo "<p>And I'll never leave again</p>";
+    echo "<p>Am            F</p>";
+    echo "<p>'Cause you are the only one</p>";
+    echo "<p>C          G</p>";
+    echo "<p>And all my friends have gone to find</p>";
+    echo "<p>Am              F</p>";
+    echo "<p>Another place to let their hearts collide</p>";
+    echo "<p>C              G</p>";
+    echo "<p>Just promise me, you'll always be a friend</p>";
+    echo "<p>Am              F</p>";
+    echo "<p>'Cause you are the only one</p>";
+    echo "<p>C          G</p>";
+    echo "<p>Take my hand and my</p>";
+    echo "<p>Am       F</p>";
+    echo "<p>Heart and soul, I will</p>";
+    echo "<p>C      G</p>";
+    echo "<p>Only have these eyes for you</p>";
+    echo "<p>Am            F</p>";
+    echo "<p>And you know, everything changes</p>";
+    echo "<p>C      G</p>";
+    echo "<p>But we'll be strangers if, we see this through</p>";
+    echo "<p>Am</p>";
+    echo "<p>You could stay within these walls</p>";
+    echo "<p>F</p>";
+    echo "<p>Or leave them </p>";
+    echo "<p>C</p>";
+    echo "<p>At my door</p>";
+    echo "<p>G</p>";
+    echo "<p>And in the air, </p>";
+    echo "<p>Am</p>";
+    echo "<p>I breathe</p>";
+    echo "<p>F</p>";
+    echo "<p>I breathe</p>";
+    echo "<p>C</p>";
+    echo "<p>I breathe</p>";
+    echo "<p>G</p>";
+    echo "<p>I breathe</p>";
+    echo "<p>[Chorus]</p>";
+    echo "<p>Am                    F</p>";
+    echo "<p>All my senses come to life</p>";
+    echo "<p>C                G</p>";
+    echo "<p>While I'm stumbling home as drunk as I</p>";
+    echo "<p>Am              F</p>";
+    echo "<p>Have ever been</p>";
+    echo "<p>C              G</p>";
+    echo "<p>And I'll never leave again</p>";
+    echo "<p>Am            F</p>";
+    echo "<p>'Cause you are the only one</p>";
+    echo "<p>C          G</p>";
+    echo "<p>And all my friends have gone to find</p>";
+    echo "<p>Am              F</p>";
+    echo "<p>Another place to let their hearts collide</p>";
+    echo "<p>C              G</p>";
+    echo "<p>Just promise me, you'll always be a friend</p>";
+    echo "<p>Am              F</p>";
+    echo "<p>'Cause you are the only one</p>";
+    echo "<p>C          G</p>";
+    echo "<p>Take my hand and my</p>";
+    echo "<p>Am       F</p>";
+    echo "<p>Heart and soul, I will</p>";
+    echo "<p>C      G</p>";
+    echo "<p>Only have these eyes for you</p>";
+    echo "<p>Am            F</p>";
+    echo "<p>And you know, everything changes</p>";
+    echo "<p>C      G</p>";
+    echo "<p>But we'll be strangers if, we see this through</p>";
+    echo "<p>Am</p>";
+    echo "<p>You could stay within these walls</p>";
+    echo "<p>F</p>";
+    echo "<p>Or leave them </p>";
+    echo "<p>C</p>";
+    echo "<p>At my door</p>";
+    echo "<p>G</p>";
+    echo "<p>And in the air, </p>";
+    echo "<p>Am</p>";
+    echo "<p>I breathe</p>";
+    echo "<p>F</p>";
+    echo "<p>I breathe</p>";
+    echo "<p>C</p>";
+    echo "<p>I breathe</p>";
+    echo "<p>G</p>";
+    echo "<p>I breathe</p>";
+
+ 
+?>
+<a href="index.php">Go Back</a>
+</body>
+</html>
