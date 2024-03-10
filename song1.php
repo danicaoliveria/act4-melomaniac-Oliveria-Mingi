c9341c2e95d21e5a82e9f1c377c96d98f5b718a6
OliveriaDanica <oliveriadanica@gmail.com>
2024-03-10 21:20:58 +0800

    song1 with lyrics

---
+++ song1.php
@@ -0,0 +1,135 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>song1</title>
+    <style>
+        body {
+            display: flex;
+            font-size: 16px;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin: 0;
+            padding: 0;
+            font-family: "Times New Roman", Times, serif;;
+            background-color: skyblue;
+        }
+
+        .content {
+            text-align: center;
+        }
+        .song-title {
+            margin-bottom: -30px;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+<pre>
+    <h1 class="song-title"> Best Part </h1>
+   <h2 style="font-size:16px;">song by Daniel Caesar</h2>
+  <?php
+ echo "[Intro: H.E.R.]" ;
+ echo "<p> D  Am  Bm  Gm</p>";
+ echo "<p>  Oh, ey </p>";
+ echo "<p> [Verse 1: H.E.R.]</p>";
+ echo "<p> D</p> " ;
+ echo "<p> You don't know babe</p>";
+ echo "<p> Am</p>";
+ echo "<p> When you hold me</p>";
+ echo "<p>     Bm</p>";
+ echo "<p> And kiss me slowly</p>";
+ echo "<p>           Gm</p>";
+ echo "<p> It's the sweetest thing</p>";
+ echo "<p> D</p>";
+ echo "<p> And it don't change</p>";
+ echo "<p> Am</p>";
+ echo "<p> If I had it my way</p>";
+ echo "<p> Bm                      Gm</p>";
+ echo "<p> You would know that you are</p>";
+ echo "[Pre-Chorus 1: H.E.R.]";
+ echo "<p> D</p.";
+ echo "<p> You're the coffee that I need in the morning</p>";
+ echo "<p> Am</p>";
+ echo "<p> You're my sunshine in the rain when it's pouring</p>";
+ echo "<p> Bm</p>";
+ echo "<p> Won't you give yourself to me</p>";
+ echo "<p>            Gm</p>";
+ echo "<p> Give it all, oh</p>";
+ echo "[Chorus: Daniel Caesar & H.E.R.]";
+ echo "<p>             D</p>";
+ echo "<p> I just wanna see</p>";
+ echo "<p>                  Am                 Bm</p>";
+ echo "<p> I just wanna see how beautiful you are</p>";
+ echo "<p>                 Gm</p>";
+ echo "<p> You know that I see it</p>";
+ echo "<p>                   D</p>";
+ echo "<p> I know you're a star</p>";
+ echo "<p>                 Am</p>";
+ echo "<p> Where you go I follow</p>";
+ echo "<p>               Bm</p>";
+ echo "<p> No matter how far</p>";
+ echo "<p>               Gm</p>";
+ echo "<p> If life is a movie</p>";
+ echo "<p>                     D*   Am*</p>";
+ echo "<p> Oh you're the best part, oh</p>";
+ echo "<p>                  Bm*  Gm*</p>";
+ echo "<p> You're the best part, oh</p>";
+ echo "<p>        D</p>";
+ echo "<p> Best part</p>";
+ echo "[Verse 2: Daniel Caesar]";
+ echo "<p>  C</p>";
+ echo "<p> It's the sunrise</p>";
+ echo "<p> Am</p>";
+ echo "<p> And those brown eyes</p>";
+ echo "<p> Bm                    Gm</p>";
+ echo "<p> You're the one that I desire</p>";
+ echo "<p> D</p>";
+ echo "<p> When we wake up</p>";
+ echo "<p> Am</p>";
+ echo "<p> And then we make love</p>";
+ echo "<p> Bm                  Gm</p>";
+ echo "<p> It makes me feel so nice</p>";
+ echo "[Pre-Chorus 2: Daniel Caesar]";
+ echo "<p> D</p>";
+ echo "<p> You're my water when I'm stuck in the desert</p>";
+ echo "<p> Am</p>";
+ echo "<p> You're the Tylenol I take when my head hurts</p>";
+ echo "<p> Bm                       Gm   D</p>";
+ echo "<p> You're the sunshine on my life</p>";
+ echo "[Chorus: Daniel Caesar & H.E.R.]";
+ echo "<p>               Am                 Bm</p>";
+ echo "<p> I just wanna see how beautiful you are</p>";
+ echo "<p>                  Gm</p>";
+ echo "<p> You know that I see it</p>";
+ echo "<p>                 D</p>";
+ echo "<p> I know you're a star</p>";
+ echo "<p>                 Am</p>";
+ echo "<p> Where you go I follow</p>";
+ echo "<p>               Bm</p>";
+ echo "<p> No matter how far</p>";
+ echo "<p>               Gm</p>";
+ echo "<p> If life is a movie</p>";
+ echo "<p>                      D     Am</p>";
+ echo "<p> Then you're the best part, oh</p>";
+ echo "<p>                Bm    Gm</p>";
+ echo "<p> You're the best part, oh</p>";
+ echo "<p>      D</p>";
+ echo "<p> Best part</p>";
+ echo "[Outro: Daniel Caesar & H.E.R.]";
+ echo "<p> D </p>";
+ echo "<p> If you love me won't you say something</p>";
+ echo "<p> Am</p>";
+ echo "<p> If you love me won't you</p>";
+ echo "<p> Won't you</p>";
+ echo "<p> Bm</p>";
+ echo "<p> If you love me won't you say something</p>";
+ echo "<p> Gm</p>";
+ echo "<p> If you love me won't you</p>";
+ echo "<p> Love me, won't you</p>";
+  
+   ?>
+   <a href="index.php">Go Back</a>
+  </body>
+</html>
