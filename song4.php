7ddf69a4f264b263466218f0426643d56736a5bc
OliveriaDanica <oliveriadanica@gmail.com>
2024-03-10 21:23:05 +0800

    song4 with lyrics

---
+++ song4.php
@@ -0,0 +1,101 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>song1</title>
+    <style>
+        body {
+            display: flex;
+            font-size: 16px;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin: 0;
+            padding: 0;
+            font-family: "Times New Roman", Times, serif;;
+            background-color: yellow;
+        }
+
+        .content {
+            text-align: center;
+        }
+        .song-title {
+            margin-bottom: -30px;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+<pre>
+    <h1 class="song-title"> Kiss Me </h1>
+   <h2 style="font-size:16px;">song by Daniel Caesar</h2>
+  <?php
+echo "<p>[Intro]</p>";
+echo "<p>C   Am   F   G</p>";
+echo "<p>[Verse 1]</p>";
+echo "<p>C             Am</p>";
+echo "<p>Kiss me, out of the bearded barley</p>";
+echo "<p>F                 G</p>";
+echo "<p>Nightly, beside the green, green grass</p>";
+echo "<p>C            Am</p>";
+echo "<p>Swing, swing, swing the spinning step</p>";
+echo "<p>F                G</p>";
+echo "<p>You wear those shoes and I will wear that dress</p>";
+echo "<p>[Pre-Chorus]</p>";
+echo "<p>Am</p>";
+echo "<p>Oh, kiss me beneath the milky twilight</p>";
+echo "<p>F</p>";
+echo "<p>Lead me out on the moonlit floor</p>";
+echo "<p>Am</p>";
+echo "<p>Lift your open hand</p>";
+echo "<p>F</p>";
+echo "<p>Strike up the band and make the fireflies dance</p>";
+echo "<p>Esus4</p>";
+echo "<p>Silver moon's sparkling</p>";
+echo "<p>[Chorus]</p>";
+echo "<p>C</p>";
+echo "<p>So kiss me</p>";
+echo "<p>[Verse 2]</p>";
+echo "<p>C               Am</p>";
+echo "<p>Kiss me, down by the broken tree house</p>";
+echo "<p>F                   G</p>";
+echo "<p>Swing me, upon its hanging tire</p>";
+echo "<p>C              Am</p>";
+echo "<p>Bring, bring, bring your flowered hat</p>";
+echo "<p>F                     G</p>";
+echo "<p>We'll take the trail marked on your father's map</p>";
+echo "<p>[Pre-Chorus]</p>";
+echo "<p>Am</p>";
+echo "<p>Oh, kiss me beneath the milky twilight</p>";
+echo "<p>F</p>";
+echo "<p>Lead me out on the moonlit floor</p>";
+echo "<p>Am</p>";
+echo "<p>Lift your open hand</p>";
+echo "<p>F</p>";
+echo "<p>Strike up the band and make the fireflies dance</p>";
+echo "<p>Esus4</p>";
+echo "<p>Silver moon's sparkling</p>";
+echo "<p>[Chorus]</p>";
+echo "<p>C</p>";
+echo "<p>So kiss me</p>";
+echo "<p>[Bridge]</p>";
+echo "<p>Am</p>";
+echo "<p>(kiss me)</p>";
+echo "<p>F</p>";
+echo "<p>Kiss me beneath the milky twilight</p>";
+echo "<p>Esus4</p>";
+echo "<p>Lead me out on the moonlit floor</p>";
+echo "<p>Am</p>";
+echo "<p>Lift your open hand</p>";
+echo "<p>F</p>";
+echo "<p>Strike up the band and make the fireflies dance</p>";
+echo "<p>Esus4</p>";
+echo "<p>Silver moon's sparkling</p>";
+echo "<p>[Chorus]</p>";
+echo "<p>C</p>";
+echo "<p>So kiss me</p>";
+
+?>
+<a href="index.php">Go Back</a>
+</body>
+</html>
