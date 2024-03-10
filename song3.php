64ffd4fe8b4587c7a57b41818e10d9a9abbd6064
OliveriaDanica <oliveriadanica@gmail.com>
2024-03-10 21:22:29 +0800

    song3 with lyrics

---
+++ song3.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>song1</title>
+    <style>
+        body {
+            display: flex;
+            font-size: 16px;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin: 0;
+            padding: 0;
+            font-family: "Times New Roman", Times, serif;;
+            background-color: lavender;
+        }
+
+        .content {
+            text-align: center;
+        }
+        .song-title {
+            margin-bottom: -30px;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+<pre>
+    <h1 class="song-title"> Daylight </h1>
+   <h2 style="font-size:16px;">song by Taylor Swift</h2>
+  <?php   
+
+ echo "<p>[Verse 1]</p>";
+    echo "<p>G</p>";
+    echo "<p>My love was as cruel as the cities I lived in</p>";
+    echo "<p>Em</p>";
+    echo "<p>Everyone looked worse in the light</p>";
+    echo "<p>G</p>";
+    echo "<p>There are so many lines that I've crossed unforgiven</p>";
+    echo "<p>Em</p>";
+    echo "<p>I'll tell you the truth, but never goodbye</p>";
+    echo "<p>[Pre-Chorus]</p>";
+    echo "<p>Am</p>";
+    echo "<p>I don't wanna look at anything else now that I saw you</p>";
+    echo "<p>D</p>";
+    echo "<p>I don't wanna think of anything else now that I thought of you</p>";
+    echo "<p>C</p>";
+    echo "<p>I've been sleeping so long in a twenty-year dark night</p>";
+    echo "<p>Em</p>";
+    echo "<p>(Now I'm wide awake)</p>";
+    echo "<p>[Chorus]</p>";
+    echo "<p>G</p>";
+    echo "<p>And I can see daylight</p>";
+    echo "<p>Em</p>";
+    echo "<p>I can see daylight</p>";
+    echo "<p>G</p>";
+    echo "<p>And I can see daylight</p>";
+    echo "<p>Em</p>";
+    echo "<p>I can see daylight</p>";
+    echo "<p>[Verse 2]</p>";
+    echo "<p>G</p>";
+    echo "<p>I'm sorry, I didn't see you there</p>";
+    echo "<p>Em</p>";
+    echo "<p>I was busy, what did you say?</p>";
+    echo "<p>G</p>";
+    echo "<p>Your voice is a whisper, but louder than mine</p>";
+    echo "<p>Em</p>";
+    echo "<p>I'll tell you the truth, but never goodbye</p>";
+    echo "<p>[Pre-Chorus]</p>";
+    echo "<p>Am</p>";
+    echo "<p>I don't wanna look at anything else now that I saw you</p>";
+    echo "<p>D</p>";
+    echo "<p>I don't wanna think of anything else now that I thought of you</p>";
+    echo "<p>C</p>";
+    echo "<p>I've been sleeping so long in a twenty-year dark night</p>";
+    echo "<p>Em</p>";
+    echo "<p>(Now I'm wide awake)</p>";
+    echo "<p>[Chorus]</p>";
+    echo "<p>G</p>";
+    echo "<p>And I can see daylight</p>";
+    echo "<p>Em</p>";
+    echo "<p>I can see daylight</p>";
+    echo "<p>G</p>";
+    echo "<p>And I can see daylight</p>";
+    echo "<p>Em</p>";
+    echo "<p>I can see daylight</p>";
+
+?>
+<a href="index.php">Go Back</a>
+</body>
+</html>
