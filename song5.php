91b6f7f4647d9e2c2a2ca1e86c3397e1b5297d43
OliveriaDanica <oliveriadanica@gmail.com>
2024-03-10 21:23:39 +0800

    song5 with lyrics

---
+++ song5.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>song1</title>
+    <style>
+        body {
+            display: flex;
+            font-size: 16px;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin: 0;
+            padding: 0;
+            font-family: "Times New Roman", Times, serif;;
+            background-color: pink;
+        }
+
+        .content {
+            text-align: center;
+        }
+        .song-title {
+            margin-bottom: -30px;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+<pre>
+    <h1 class="song-title"> The Archer </h1>
+   <h2 style="font-size:16px;">song by Taylor Swift</h2>
+  <?php
+  echo "<p>[Verse 1]</p>";
+  echo "<p>Em</p>";
+  echo "<p>Combat</p>";
+  echo "<p>G</p>";
+  echo "<p>I'm ready for combat</p>";
+  echo "<p>Am</p>";
+  echo "<p>I say I don't want that, but what if I do?</p>";
+  echo "<p>Em</p>";
+  echo "<p>'Cause cruelty wins in the movies</p>";
+  echo "<p>G</p>";
+  echo "<p>I've got a hundred thrown-out speeches I almost said to you</p>";
+  echo "<p>Am</p>";
+  echo "<p>Easy they come, easy they go</p>";
+  echo "<p>C</p>";
+  echo "<p>I jump from the train, I ride off alone</p>";
+  echo "<p>Am</p>";
+  echo "<p>I never grew up, it's getting so old</p>";
+  echo "<p>Em</p>";
+  echo "<p>Help me hold on to you</p>";
+  echo "<p>[Chorus]</p>";
+  echo "<p>Em</p>";
+  echo "<p>I've been the archer</p>";
+  echo "<p>G</p>";
+  echo "<p>I've been the prey</p>";
+  echo "<p>Am</p>";
+  echo "<p>Screaming, who could ever leave me, darling?</p>";
+  echo "<p>Em</p>";
+  echo "<p>But who could stay?</p>";
+  echo "<p>[Verse 2]</p>";
+  echo "<p>Em</p>";
+  echo "<p>Darting, I'm dodging, I'm longing</p>";
+  echo "<p>G</p>";
+  echo "<p>To be near you</p>";
+  echo "<p>Am</p>";
+  echo "<p>But you're right here</p>";
+  echo "<p>Em</p>";
+  echo "<p>In my head</p>";
+  echo "<p>G</p>";
+  echo "<p>'Cause kindness is weakness</p>";
+  echo "<p>Am</p>";
+  echo "<p>Or worse, you're the end of all</p>";
+  echo "<p>Em</p>";
+  echo "<p>The rest of my life</p>";
+  echo "<p>G</p>";
+  echo "<p>Sign my name (Sign my name)</p>";
+  echo "<p>Am</p>";
+  echo "<p>To the scars on your heart</p>";
+  echo "<p>C</p>";
+  echo "<p>I want to love, love</p>";
+  echo "<p>Am</p>";
+  echo "<p>And you call it a gift</p>";
+  echo "<p>Em</p>";
+  echo "<p>Easy they come, easy they go</p>";
+  echo "<p>C</p>";
+  echo "<p>I jump from the train, I ride off alone</p>";
+  echo "<p>Am</p>";
+  echo "<p>I never grew up, it's getting so old</p>";
+  echo "<p>Em</p>";
+  echo "<p>Help me hold on to you</p>";
+  echo "<p>[Chorus]</p>";
+  echo "<p>Em</p>";
+  echo "<p>I've been the archer</p>";
+  echo "<p>G</p>";
+  echo "<p>I've been the prey</p>";
+  echo "<p>Am</p>";
+  echo "<p>Screaming, who could ever leave me, darling?</p>";
+  echo "<p>Em</p>";
+  echo "<p>But who could stay?</p>";
+
+?>
+<a href="index.php">Go Back</a>
+</body>
+</html>
